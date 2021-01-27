4cea8abcc74948bb34b6992bd9e95067adeb0708
serenomoon <61322799+serenomoon@users.noreply.github.com>
2021-01-27 16:42:14 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+  email : String,
+  password: String
+});
+
+const User = mongoose.model("User",userSchema);
+
+
+app.route("/")
+  .get(function(req,res){
+    res.render("home");
+  });
+
+
+app.route("/login")
+  .get(function(req,res){
+    res.render("login");
+  })
+
+  .post(function(req,res){
+    const username= req.body.username;
+    const password= req.body.password;
+
+    User.findOne({email:username},function(err,foundUser){
+      if (err){
+        console.log(err);
+      }else{
+        if(foundUser){
+          bcrypt.compare(password,foundUser.password,function(err,result){
+            if (result === true){
+              res.render("secrets");
+            }
+          });  
+        }
+      }
+    });
+  });
+
+
+app.route("/register")
+  .get(function(req,res){
+    res.render("register");
+  })
+
+  .post(function(req,res){
+
+    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    });
+    });
+  });
+
+
+
+
+
+
+app.listen(3000,function(){
+  console.log("Server is running on port 3000.");
+})
